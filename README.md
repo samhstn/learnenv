22acd3400d9d49fef6d1f3b2c79fe695cfda72c7
shouston3 <shouston3@me.com>
2016-02-06 11:22:41 +0000

    added other files and a readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# A german language learning environment
+
+## What?
+
+* Uses an array of 400 verbs, 200 in german, 200 in english
+* Has an option to select the direction of translation
+* Goes through 5 words at a time, noting where you are out of the 200
+* Has an option to show/hide all, next and previous words
+
+## Why?
+
+I would like to learn German and wanted a learning environment to do so
+
+## Current functionality
+
+* The home page has the option of two translation directions, but only de -> en works
+* The next and prev buttons work quite well, but the prev button does not go back to the beginning when at the start
+* Has a counter which displays where you are in the 200 words
+* The array of 400 words is in the correct order and outputs when needed, what more can you want from an array
+
+## Future functionality
+
+* All buttons work fine including the option of both translation directions
+* A box to check if you got an answer wrong and then a separate test for the answers you got wrong
+* When familiar with node.js, be able to use backend to save mistakes log as well as reset mistakes log
+
+## Tasks
+
+* Refactor js to be more concise more for loops and ternary functions
+* Refactor css and add mqueries to work on mobile
+* Neeten up the css so there are no overlaps of divs and perhaps add smooth transitions
+* Set up other translation direction button
+* Sort out prev button to be able to go to the end from beginning
+* Sort out counter to only display positive values
+* Fix hide show buttons to detect what is currently showing
+
+## Stretch Tasks
+
+* Add backend
+* Add mistakes log with mistakes test option
+* Add more verbs
+* Add a separate nouns option
--- script.js
+++ script.js
@@ -7,44 +7,104 @@ for(i=0;i<nums.length;i++){
 }
 function show(){
     for(i=0;i<5;i++){
-        document.getElementsByClassName("right")[i].classList.add("hide");
+        document.getElementsByClassName("english")[i].classList.add("hide");
     }
     document.getElementById("hide").classList.remove("hide");
     document.getElementById("show").classList.add("hide");
 }
 function hide(){
     for(i=0;i<5;i++){
-        document.getElementsByClassName("right")[i].classList.remove("hide");
+        document.getElementsByClassName("english")[i].classList.remove("hide");
     }
     document.getElementById("show").classList.remove("hide");
     document.getElementById("hide").classList.add("hide");
 }
 function createDivs(){
     var x = 1;
-    for(i=0;i<numbers.length;i++){
+    for(i=0;i<10;i++){
         var node = document.createElement('div');
-        node.innerHTML=input[i];
+        node.innerHTML=vocab[i];
         node.className='word';
         document.body.appendChild(node);
-        document.getElementsByTagName('div')[i+5].classList.add(numbers[i]);
+        document.getElementsByTagName('div')[i+8].classList.add(numbers[i]);
         if(x>0){
-            document.getElementsByClassName(numbers[i])[0].classList.add('left');
+            document.getElementsByClassName(numbers[i])[0].classList.add('german');
+            document.getElementsByClassName(numbers[i])[0].classList.add('hide');
         }
         else{
-            document.getElementsByClassName(numbers[i])[1].classList.add('right');
+            document.getElementsByClassName(numbers[i])[1].classList.add('english');
             document.getElementsByClassName(numbers[i])[1].classList.add('hide');
         }
         x*=-1;
     }
-    document.getElementsByClassName("left")[0].onclick=function(){hideone();};
-    document.getElementsByClassName("left")[1].onclick=function(){hidetwo();};
-    document.getElementsByClassName("left")[2].onclick=function(){hidethree();};
-    document.getElementsByClassName("left")[3].onclick=function(){hidefour();};
-    document.getElementsByClassName("left")[4].onclick=function(){hidefive();};
+    document.getElementById("counter").innerHTML=counter/2-4 + " - " + counter/2 + " of 200";
+    document.getElementsByClassName("german")[0].onclick=function(){hideone();};
+    document.getElementsByClassName("german")[1].onclick=function(){hidetwo();};
+    document.getElementsByClassName("german")[2].onclick=function(){hidethree();};
+    document.getElementsByClassName("german")[3].onclick=function(){hidefour();};
+    document.getElementsByClassName("german")[4].onclick=function(){hidefive();};
 }
 
-function hideone(){document.getElementsByClassName("right")[0].classList.toggle("hide");}
-function hidetwo(){document.getElementsByClassName("right")[1].classList.toggle("hide");}
-function hidethree(){document.getElementsByClassName("right")[2].classList.toggle("hide");}
-function hidefour(){document.getElementsByClassName("right")[3].classList.toggle("hide");}
-function hidefive(){document.getElementsByClassName("right")[4].classList.toggle("hide");}
+function hideone(){document.getElementsByClassName("english")[0].classList.toggle("hide");}
+function hidetwo(){document.getElementsByClassName("english")[1].classList.toggle("hide");}
+function hidethree(){document.getElementsByClassName("english")[2].classList.toggle("hide");}
+function hidefour(){document.getElementsByClassName("english")[3].classList.toggle("hide");}
+function hidefive(){document.getElementsByClassName("english")[4].classList.toggle("hide");}
+
+var counter = 10;
+
+function eng(){
+    document.getElementById("home").classList.remove("hide");
+    document.getElementById("show").classList.remove("hide");
+    document.getElementById("next").classList.remove("hide");
+    document.getElementById("prev").classList.remove("hide");
+    document.getElementById("ende").classList.add("hide");
+    document.getElementById("deen").classList.add("hide");
+    document.getElementsByTagName("h1")[0].classList.add("hide");
+    document.getElementsByTagName("h2")[0].classList.add("hide");
+}
+function de(){
+    document.getElementById("home").classList.remove("hide");
+    document.getElementById("show").classList.remove("hide");
+    document.getElementById("next").classList.remove("hide");
+    document.getElementById("prev").classList.remove("hide");
+    document.getElementById("ende").classList.add("hide");
+    document.getElementById("deen").classList.add("hide");
+    document.getElementsByTagName("h1")[0].classList.add("hide");
+    document.getElementsByTagName("h2")[0].classList.add("hide");
+    for(i=0;i<5;i++){
+        document.getElementsByClassName("german")[i].classList.remove("hide");
+    }
+    document.getElementById("counter").classList.remove("hide");
+}
+function next(){
+    document.getElementsByClassName("german")[0].innerHTML=vocab[(0+counter)%400];
+    document.getElementsByClassName("german")[1].innerHTML=vocab[(2+counter)%400];
+    document.getElementsByClassName("german")[2].innerHTML=vocab[(4+counter)%400];
+    document.getElementsByClassName("german")[3].innerHTML=vocab[(6+counter)%400];
+    document.getElementsByClassName("german")[4].innerHTML=vocab[(8+counter)%400];
+    document.getElementsByClassName("english")[0].innerHTML=vocab[(1+counter)%400];
+    document.getElementsByClassName("english")[1].innerHTML=vocab[(3+counter)%400];
+    document.getElementsByClassName("english")[2].innerHTML=vocab[(5+counter)%400];
+    document.getElementsByClassName("english")[3].innerHTML=vocab[(7+counter)%400];
+    document.getElementsByClassName("english")[4].innerHTML=vocab[(9+counter)%400];
+    for(i=0;i<5;i++){
+        document.getElementsByClassName("english")[i].classList.add("hide");
+    }
+    counter=(counter+10)%400;
+    document.getElementById("counter").innerHTML=counter/2-4 + " - " + counter/2 + " of 200";
+}
+function prev(){
+    if(counter<10){
+        counter+=380;
+        next();
+    }
+    else if(counter>400) {
+        counter-=420;
+        next();
+    }
+    else {
+        counter-=20;
+        next();
+    }
+}
